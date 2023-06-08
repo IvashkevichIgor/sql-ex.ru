ac7797a576340b87cd8fb2a25e90d29889d1cabf
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-06-08 07:20:08 +0300

    add ex_063

---
+++ SELECT_learning_stage/ex_061-080/ex_063.sql
@@ -0,0 +1,9 @@
+/* Определить имена разных пассажиров, когда-либо летевших 
+ * на одном и том же месте более одного раза. */
+
+SELECT name
+  FROM Passenger 
+ WHERE ID_psg IN(SELECT ID_psg 
+                   FROM Pass_in_trip
+                  GROUP BY ID_psg
+                 HAVING COUNT(place) - COUNT(DISTINCT place) >= 1);
